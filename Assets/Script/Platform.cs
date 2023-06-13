91c2573da54e2cb323fce592b43f52ad0faffe5d
Mustafa Kan <79532179+mustafakan01@users.noreply.github.com>
2023-06-13 22:10:23 +0300

    Ground

--- Assets/Script/Platform.cs
+++ Assets/Script/Platform.cs
@@ -8,6 +8,7 @@ public class Platform : MonoBehaviour
     BoxCollider2D box;
     public float groundWithd;
     public static Rigidbody2D rb;
+    Vector2 vec;
 
     
     void Start()
@@ -15,11 +16,20 @@ public class Platform : MonoBehaviour
         box = GetComponent<BoxCollider2D>();
         rb = GetComponent<Rigidbody2D>();
         groundWithd = box.size.x;
+        vec=transform.position;
     }
 
     
     void Update()
     {
         transform.position=new Vector2(transform.position.x-speed*Time.deltaTime, transform.position.y);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Obje"))
+        {
+            transform.position = vec;
+        }
     }
 }
